cbc6cecc6698be90b27a79892d40ec0740b194b8
Sivakumaran Vithursan <sivithu93240@gmail.com>
2019-10-15 17:10:52 +0200

    First commit with login page

---
+++ StarWithMyWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWithMyWatch
+//
+//  Created by Vithursan Sivakumaran on 15/10/2019.
+//  Copyright © 2019 Vithursan Sivakumaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
